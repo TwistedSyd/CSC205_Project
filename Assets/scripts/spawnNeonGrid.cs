d811de42176f0f43f9e66a408582abc5a70c5b80
Daniel Frankcom <danielfrankcom@gmail.com>
2017-03-24 16:13:04 -0700

    Added neon lines to grid, and created models folder
    There is now a script that spawns neon lines on the plane when the world
    is camera is initiated. These are a prefab that I found on the Unity
    store, as it would seem that creating neon lights any other way is
    prohibited without the Pro version of Unity.

---
+++ Assets/scripts/spawnNeonGrid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnNeonGrid : MonoBehaviour {
+
+    public int gridDimension;
+    public Transform line; //Stores neon line prefab
+
+
+	void Start () {
+
+        int max = gridDimension / 2 + 1;
+        for (int i = -gridDimension / 2; i < max; i++) //Loop through all lines to be generated
+        {
+            for(int j = 0; j < 2; j++)  //Both directions
+            {
+                if(j == 0)
+                {
+                    Instantiate(line, new Vector3(i, 0.01f, 0), Quaternion.Euler(0,0,0), null); //Vertical lines
+                }
+                else
+                {
+                    Instantiate(line, new Vector3(0, 0.01f, i), Quaternion.Euler(0, 90, 0), null); //Horizontal lines
+                }
+            }
+        }
+
+	}
+	
+}
