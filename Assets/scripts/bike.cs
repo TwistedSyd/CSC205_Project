e6ba29faced471a50c4f90cd1f1e5f98658c7adc
Daniel Frankcom <danielfrankcom@gmail.com>
2017-03-24 17:09:02 -0700

    Added initial bike movement scripts

---
+++ Assets/scripts/bike.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bike : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.forward = new Vector3(1, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void moveForward()
+    {
+        transform.position += transform.forward * Time.deltaTime;
+    }
+}
---
+++ Assets/scripts/getInputs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getInputs : MonoBehaviour {
+    
+    private bike cycle;
+
+	// Use this for initialization
+	void Start () {
+
+        cycle = GetComponentInParent<bike>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Comma) || Input.GetKey(KeyCode.UpArrow))
+        {
+            cycle.moveForward();
+        }
+
+	}
+}
