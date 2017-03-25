c726288081f952b53c6e5b75ae0243767a949d3b
Daniel Frankcom <danielfrankcom@gmail.com>
2017-03-24 18:27:31 -0700

    Camera Following and rotation implemented
    This is very basic, we should definately implement bike rolling to the
    side when we turn, as well as subtle camera turning delay so that we can
    see the side of the bike a bit.

---
+++ Assets/scripts/camera/cameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow : MonoBehaviour
+{
+
+    private GameObject player;
+    private Quaternion lastRotation;
+
+    void Start()
+    {
+
+        player = GameObject.Find("redBike");
+        lastRotation = player.transform.rotation;
+
+    }
+
+
+    void LateUpdate()
+    {
+
+        transform.position = player.transform.position;
+        transform.position -= player.transform.forward;
+        transform.position += 2 * player.transform.up;
+
+        if (player.transform.rotation != lastRotation)
+        {
+            var forwardA = lastRotation * Vector3.forward; //Vector for rotation
+            var forwardB = player.transform.rotation * Vector3.forward;
+
+            var angleA = Mathf.Atan2(forwardA.x, forwardA.z) * Mathf.Rad2Deg; //Angle for vector
+            var angleB = Mathf.Atan2(forwardB.x, forwardB.z) * Mathf.Rad2Deg;
+
+            var angleDiff = Mathf.DeltaAngle(angleA, angleB); //Signed angle difference
+
+            transform.RotateAround(transform.position, player.transform.up, angleDiff);
+            lastRotation = player.transform.rotation;
+        }
+    }
+}
